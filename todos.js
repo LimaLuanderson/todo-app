1e84c1535458bdccef095aeef13af62c3435f243
Francisco Luanderson <luanlima655@gmail.com>
2021-02-21 20:00:31 -0300

    initial commit

---
+++ todos.js
@@ -0,0 +1,41 @@
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input');
+var buttonElement = document.querySelector('#app button');
+
+var todos = ['java', 'ruby', 'kotlin'];
+
+function renderTodos() {
+    listElement.innerHTML = '';
+    
+    todos.forEach(todo => {
+        var li = document.createElement('li');
+        li.innerHTML = `${todo}`;
+
+        var deleteButton = document.createElement('button');
+        deleteButton.innerHTML = '<i class="fas fa-trash-alt"></i>';
+
+        var id = todos.indexOf(todo);
+        deleteButton.setAttribute('onclick', `deleteTodo(${id})`)
+
+        listElement.appendChild(li);
+        li.appendChild(deleteButton);
+    })
+
+}
+
+renderTodos();
+
+function addTodo() {
+    var todoText = inputElement.value;
+    todos.push(todoText);    
+    inputElement.value = '';
+
+    renderTodos();
+}
+
+buttonElement.onclick = addTodo;
+
+function deleteTodo(id) {
+    todos.splice(id, 1);
+    renderTodos();
+}
